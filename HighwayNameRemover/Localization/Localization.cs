0b2922b1761d03089b81e25ce9007a38be2cf6fe
kosch104 <konstantin-schwarze@outlook.de>
2024-03-09 20:36:29 +0100

    Clean up and remove unneeded code

--- HighwayNameRemover/Localization/Localization.cs
+++ HighwayNameRemover/Localization/Localization.cs
@@ -1,50 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 using Colossal.IO.AssetDatabase;
-using Colossal.Json;
-using UnityEngine;
 
-namespace HighwayNameRemover
+namespace HighwayNameRemover.Localization
 {
 	public class Localization
 	{
-		internal static Dictionary<string, Dictionary<string, string>> localization;
-
 		internal static void AddCustomLocal(LocaleAsset localeAsset)
 		{
-			if(localization is null) LoadLocalization();
-
-			string loc = localeAsset.localeId;
-
-			if(!localization.ContainsKey(loc)) // Fallback language
-				loc = "en-US";
-
-            foreach(string key in localization[loc].Keys)
-            {
-                if(localeAsset.data.entries.ContainsKey(key))
-	                localeAsset.data.entries[key] = localization[loc][key];
-                else
-	                localeAsset.data.entries.Add(key, localization[loc][key]);
-
-                if(!key.Contains(":")) continue;
-
-                string[] parts = key.Split(":");
-                if (int.TryParse(parts[1], out int n))
-                {
-	                n++;
-	                if (localeAsset.data.indexCounts.ContainsKey(parts[0]))
-	                {
-		                if (localeAsset.data.indexCounts[parts[0]] != n) // Was <
-		                {
-			                localeAsset.data.indexCounts[parts[0]] = n;
-		                }
-	                }
-	                else localeAsset.data.indexCounts.Add(parts[0], n);
-                }
-            }
-
             List<string> typesToRemove = new List<string>();
 			var cfg = HighwayNameRemoverController._config;
 			if (cfg.HideStreetNames)
@@ -73,43 +36,5 @@ namespace HighwayNameRemover
 				localeAsset.data.entries[key] = "         ";
 			}
 		}
-
-		private static void LoadLocalization()
-		{
-			var assembly = Assembly.GetExecutingAssembly();
-			var resourceNames = assembly.GetManifestResourceNames();
-
-			Dictionary<string, Dictionary<string, string>> dictionary = new();
-			foreach (var resourceName in resourceNames)
-			{
-				if (resourceName.EndsWith(".json") && resourceName.Contains(".embedded.Localization."))
-				{
-					using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-					{
-						using (StreamReader reader = new StreamReader(stream))
-						{
-							string result = reader.ReadToEnd();
-							LocalizationLocaleJS loc = Decoder.Decode(result).Make<LocalizationLocaleJS>();
-							// Get the locale from the filename
-							var locale = resourceName.Split(".embedded.Localization.")[1].Split(".json")[0];
-							dictionary.Add(locale, loc.Localization);
-						}
-					}
-				}
-			}
-			localization = dictionary;
-		}
-	}
-
-	[Serializable]
-	public class LocalizationLocaleJS
-	{	
-		public Dictionary<string, string> Localization;
-
-	}
-
-	public class LocalizationJS
-	{
-		public Dictionary<string, Dictionary<string, string>> Localization = [];
 	}
 }
--- HighwayNameRemover/Patches/HighwayNameRemoverPatches.cs
+++ HighwayNameRemover/Patches/HighwayNameRemoverPatches.cs
@@ -2,36 +2,16 @@
 using HarmonyLib;
 using Colossal.Localization;
 using Colossal.IO.AssetDatabase;
+using HighwayNameRemover.Localization;
 
 namespace HighwayNameRemover.Patches
 {
     [HarmonyPatch(typeof(LocalizationManager), "AddLocale", typeof(LocaleAsset))]
     internal class LocalizationManager_AddLocale
     {
-        static readonly LocalizationJS localizationJS = new();
         static void Prefix(LocaleAsset asset)
         {
-            Localization.AddCustomLocal(asset);
-
-            foreach(string key in asset.data.entries.Keys)
-            {
-                if(!localizationJS.Localization.ContainsKey(asset.localeId))
-                {
-                    localizationJS.Localization.Add(asset.localeId, []);
-                }
-                if(!localizationJS.Localization[asset.localeId].ContainsKey(key)) localizationJS.Localization[asset.localeId].Add(key, asset.data.entries[key]);
-            }
-
-            Dictionary<string, Dictionary<string, int>> localeIndex = [];
-
-            foreach(string key in asset.data.indexCounts.Keys)
-            {
-                if(!localeIndex.ContainsKey(asset.localeId))
-                {
-                    localeIndex.Add(asset.localeId, []);
-                }
-                if(!localeIndex[asset.localeId].ContainsKey(key)) localeIndex[asset.localeId].Add(key, asset.data.indexCounts[key]);
-            }
+            Localization.Localization.AddCustomLocal(asset);
         }
     }
 }
